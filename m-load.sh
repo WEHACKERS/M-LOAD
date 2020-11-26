3c4eb81355b033d4722a01cdcd4b6a88d5771500
WE-HACKERS <75081276+WEHACKERS@users.noreply.github.com>
2020-11-26 09:19:29 -0500

    M-LOAD UPDATER TOOL INSTALLER
    THIS TOOL IS VERY ADVANCED TOOL DOWNLOADING TOOL

---
+++ m-load.sh
@@ -0,0 +1,86 @@
+#!Script  author DAYSHEN HACKERS
+#By dayshen hackers
+#Be a creative person not copy paster
+
+###################################
+cyan='\e[0;36m'
+
+lightgreen='\e[1;32m'
+
+red='\e[1;31m'
+
+yellow='\e[1;33m'
+###################################
+
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo -e "\e[1m\e[31m\
+           
+              ██╗hacker hacker dayshen iam a ezthical hacker      ██████╗  █████╗ ██████╗
+           ╚══█╝   ██║     ██╔═══██╗██╔══██╗██╔══██╗
+             ║█████╗██║     ██║   ██║███████║██║  ██║this tool use for your hacking
+              ██║╚════╝██║     ██║   ██║██╔══██║██║  ██║
+              ██║      ███████╗╚██████╔╝██║  ██║██████╔╝
+              ╚═╝      ╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ v 1.1"
+echo " "  
+echo -e  "                  \e[1m\e[32m▂▃▄▅▆▇▓▒░Coded By DAYSHEN HACKERS░▒▓▇▆▅▄▃▂"
+echo "                      -------------------------"
+echo -e  "            \e[1m\e[32m--------->[\e[31mDEEPED- dayshen hackers\e[1m\e[32m]<----------"  
+echo " "
+echo ""
+sleep 8.0
+apt install mpv -y
+linux-setup-storage
+cd  /data/data/com.linux/files/usr/etc
+
+rm bash.bashrc
+
+ls
+
+cd $HOME
+
+ls 
+
+cd M-LOAD
+
+ls
+
+cp bash.bashrc /data/data/com.linux/files/usr/etc
+
+ls
+
+cd $HOME
+
+ls
+
+cd M-LOAD
+
+ls
+
+cp scifi.mp3 /sdcard
+
+clear
+bash packages.sh
+clear
+sleep 4.0
+bash ins.sh 
+sleep 5.0
+clear
+sleep 5.0
+bash thanks.sh 
+sleep 2.0 
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo "             "EXIT FROM TERMUX or linux  AND RE OPEN IT AFTER  THANKS TO DOWNLOAD M-LOAD 5 SECONDS |lolcat 
+echo " "
+echo " "
+sleep 3.0
+cd $HOME
